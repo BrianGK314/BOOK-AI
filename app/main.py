8b8d13c44ea2ab9b4e492fe6ce341597dc357e5f
Brian Gitau <briangitau02@gmail.com>
2022-03-31 10:14:32 +0300

    initial committ

--- app/main.py
+++ app/main.py
@@ -11,8 +11,33 @@ import logging
 import asyncio
 
 
+import logging
+import traceback
+
+from flask import Flask, jsonify
+from werkzeug.exceptions import HTTPException
+
+
+
 UPLOAD_FOLDER = "app/images"
 app = Flask(__name__)
+
+
+logger = logging.getLogger()
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(error):
+    error_dict = {
+        'code': error.code,
+        'description': error.description,
+        'stack_trace': traceback.format_exc() 
+    }
+    log_msg = f"HTTPException {error_dict.code}, Description: {error_dict.description}, Stack trace: {error_dict.stack_trace}"
+    logger.log(msg=log_msg)
+    response = jsonify(error_dict)
+    response.status_code = error.code
+    return response
+
 
 app.logger.setLevel(logging.ERROR)
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
